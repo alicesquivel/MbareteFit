2102e68a77791c85a4d840e1a1383a9f4e48a1e2
alicesquivel <ace6qv@mail.missouri.edu>
2025-07-01 13:20:47 -0500

    Updated the base path to the specific repository name

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // This base path is now set to your specific repository name
+  base: '/MbareteFit/',
   plugins: [react()],
-  base: '/mbaretefit/', // <-- Add this line
 })
