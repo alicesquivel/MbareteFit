243ca2555f74864d29cd365966b9af02b01eafd8
alicesquivel <ace6qv@mail.missouri.edu>
2025-07-01 16:37:30 -0500

    firebase bug

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,19 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
+import { firebaseConfig } from "../firebase-config";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCzzeNhnhMx4Hf5hJW_vz2V_jgindTysvs",
-    authDomain: "mbaretefit-b6c57.firebaseapp.com",
-    databaseURL: "https://mbaretefit-b6c57-default-rtdb.firebaseio.com",
-    projectId: "mbaretefit-b6c57",
-    storageBucket: "mbaretefit-b6c57.appspot.com",
-    messagingSenderId: "33965493200",
-    appId: "1:33965493200:web:07a9531bf2382755e007d5",
-    measurementId: "G-W8JBF51MCE"
-};
-
+// Initialize Firebase app
 const app = initializeApp(firebaseConfig);
 
+// Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
+
+// Initialize Realtime Database and get a reference to the service
 export const db = getDatabase(app);
